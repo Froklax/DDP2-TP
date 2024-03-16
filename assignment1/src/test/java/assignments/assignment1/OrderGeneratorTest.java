59a75cf904c5c0ae3f689d87a9ee0305a09f7337
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-03-16 23:13:03 +0700

    modified test case assignment1 to accomodate the changes made to OrderGenerator

--- assignment1/src/test/java/assignments/assignment1/OrderGeneratorTest.java
+++ assignment1/src/test/java/assignments/assignment1/OrderGeneratorTest.java
@@ -11,12 +11,15 @@ public class OrderGeneratorTest {
 
     @Test
     public void testGenerateBill1() {
-        String solution = "Bill:\n" + //
-                "Order ID: HOLY1802202453C3\n" + //
-                "Tanggal Pemesanan: 18/02/2024\n" + //
-                "Lokasi Pengiriman: S\n" + //
-                "Biaya Ongkos Kirim: Rp 40.000\n" + //
-                "";
-        assertEquals(solution, OrderGenerator.generateBill("HOLY1802202453C3", "s"));
+        String solution = "Bill:\n" +
+                "Order ID: HOLY1802202453C3\n" +
+                "Tanggal Pemesanan: 18/02/2024\n" +
+                "Restaurant: Holycow!\n" +
+                "Lokasi Pengiriman: S\n" +
+                "Status Pemesanan: Selesai\n" + // Ganti dengan status pesanan yang sesuai
+                "Pesanan:\n" + "1. Pisang 20000" +
+                "Biaya Ongkos Kirim: Rp " + 40000 + "\n" +
+                "Total Biaya: Rp " + (20000 + 40000) + "\n";
+        assertEquals(solution, OrderGenerator.generateBill("HOLY1802202453C3", "Holycow!", "s", "Selesai", "1. Pisang 20000", 20000));
     }
 }
